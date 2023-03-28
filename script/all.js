c79e4e71b5696078967cb8acb76f9a46a27fe488
Adnan <77362547+adddeveloper@users.noreply.github.com>
2023-03-27 22:50:22 -0300

    update

--- script/all.js
+++ script/all.js
@@ -4,18 +4,34 @@ function go(x){
     a.click();
 }
 
+function capitalizeFirstLetter(str) {
+    var whole="";
+    var arr = str.split(" ");
+    arr.forEach(e=>{
+        var rr = e.split('')
+        rr.shift()
+        rr.unshift(e.split('')[0].toUpperCase())
+        whole += " "+ rr.join("")
+    })
+    return whole;
+}
 
 
 var url = ['/10/10.json', '/5/5.json', '/3/3.json'];
 var ddata=[];
 var holder = document.getElementById("holder__");
+
+function scrollToTarget() {
+    holder.scrollIntoView({ behavior: 'smooth' });
+}
+
 function upload(x){
     x.forEach(e=>{
         holder.innerHTML+=
     '<div onclick="go(\''+e.url+'\')" style="background-image: url(/style/img/'+e.img+');" class="crd">'+
         '<img src="/style/img/'+e.img+'"></img>'+
         '<div>'+
-                '<h1>'+e.title+'</h1>'+
+                '<h1>'+capitalizeFirstLetter(e.title)+'</h1>'+
                 '<p>'+e.description+'</p>'+
         '</div>'+
     '</div>';
